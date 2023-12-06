572a875b0a574fc8b11f13780574e874cb328500
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-06 17:17:52 +0000

    Remove Existed hotel codes to place generate one

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -11,12 +11,12 @@ document.querySelectorAll('.special').forEach(function (special) {
   const [latitude, longitude] = special.getAttribute('coor').split(',').map(Number);
 
   // Create a marker
-  var marker = new mapboxgl.Marker()
+  const marker = new mapboxgl.Marker()
     .setLngLat([longitude, latitude])
     .addTo(map);
 
   // Create a popup
-  var popup = new mapboxgl.Popup({ offset: 25 })
+  const popup = new mapboxgl.Popup({ offset: 25 })
     .setText(special.querySelector('h3').textContent) // Set the popup text to the h3 content
     .setMaxWidth('300px'); // Optional: Set max width of the popup
 
