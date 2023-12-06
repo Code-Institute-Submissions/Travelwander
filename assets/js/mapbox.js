e4b0eed121c451485cb4b1fe3f62d1374f1907bb
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-06 18:35:34 +0000

    Mix two script of mapbox.js to find coor

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -1,30 +1,118 @@
-//mbox
-mapboxgl.accessToken = 'token.';
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11', //  map style
-  center: [-82.25306449222539, 9.40445610762273], // center
-  zoom: 11 // set zoom level
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the country parameter from the URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const country = urlParams.get('country');
+
+  // Fetch hotels based on the selected country
+  fetch('hotels_data.json')
+      .then(response => response.json())
+      .then(data => {
+          const hotels = getHotelsByCountry(data, country);
+
+          // Display hotels in the container
+          displayHotels(hotels);
+
+          // Initialize the map after displaying hotels
+          initializeMap();
+      })
+      .catch(error => console.error('Error fetching hotels data:', error));
 });
 
-document.querySelectorAll('.special').forEach(function (special) {
-  const [latitude, longitude] = special.getAttribute('coor').split(',').map(Number);
+// Function to filter hotels by country from your JSON data
+function getHotelsByCountry(data, country) {
+  // Your logic to filter hotels based on the country
+  // Return an array of hotels
+  return data.filter(hotel => hotel.country === country);
+}
 
-  // Create a marker
-  const marker = new mapboxgl.Marker()
-    .setLngLat([longitude, latitude])
-    .addTo(map);
+// Function to display hotels in the container
+function displayHotels(hotels) {
+  const hotelsContainer = document.getElementById('hotelsContainer');
+  hotelsContainer.classList.add('special-container');
+  const cordiants_list = [];
 
-  // Create a popup
-  const popup = new mapboxgl.Popup({ offset: 25 })
-    .setText(special.querySelector('h3').textContent) // Set the popup text to the h3 content
-    .setMaxWidth('300px'); // Optional: Set max width of the popup
+  hotels.forEach(hotel => {
+      // Create elements to display hotel information
+      const hotelDiv = document.createElement('div');
+      hotelDiv.classList.add('special'); // Add 'special' class
+      hotelDiv.setAttribute('coor', hotel.coordinates); // Add 'coor' attribute
+      hotelDiv.dataset.coordinates = hotel.coordinates;
 
-  // Add the popup to the marker
-  marker.setPopup(popup);
+      cordiants_list.push(hotel.coordinates);
+      const spNotesDiv = document.createElement('div');
+      spNotesDiv.classList.add('sp_notes');
 
-  // Optional: Add event listener for showing the popup on marker click
-  marker.getElement().addEventListener('click', function () {
-    marker.togglePopup();
+      const moreInfoButton = document.createElement('button');
+      moreInfoButton.setAttribute('type', 'button');
+      moreInfoButton.setAttribute('onclick', 'showHotelDetails(this)');
+      moreInfoButton.textContent = 'More Info';
+
+      const h4Adventure = document.createElement('h4');
+      h4Adventure.innerHTML = `<i class="fa-solid fa-tag" style="color: #e40754;"></i> adventure <br> price : ${hotel.price} ${hotel.currency}`;
+
+      const h4Stars = document.createElement('h4');
+      h4Stars.innerHTML = '<i class="fa-solid fa-star" style="color: #fbe709;"></i>'.repeat(hotel.rating);
+
+      const h3HotelName = document.createElement('h3');
+      h3HotelName.textContent = hotel.name;
+
+      const pLocation = document.createElement('p');
+      pLocation.innerHTML = `<i class="fa-solid fa-location-dot"></i>${hotel.location}<br> Flight + hotel <br> ${hotel.details.join('<br>')} <br>`;
+
+      // Append elements to the spNotesDiv
+      spNotesDiv.appendChild(moreInfoButton);
+      spNotesDiv.appendChild(h4Adventure);
+      spNotesDiv.appendChild(h4Stars);
+      spNotesDiv.appendChild(h3HotelName);
+      spNotesDiv.appendChild(pLocation);
+
+      const spImgDiv = document.createElement('div');
+      spImgDiv.classList.add('sp_img');
+
+      const imgHotel = document.createElement('img');
+      imgHotel.src = hotel.imageSrc;
+      imgHotel.alt = `${hotel.name} image`;
+
+      // Append elements to the hotelDiv
+      hotelDiv.appendChild(spNotesDiv);
+      hotelDiv.appendChild(spImgDiv);
+      spImgDiv.appendChild(imgHotel);
+
+      // Append the hotelDiv to the hotelsContainer
+      hotelsContainer.appendChild(hotelDiv);
   });
-});
+}
+
+// Map initialization function
+function initializeMap() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWFpc2FtMjAwNCIsImEiOiJjbHBsZTJrYTcwM3lmMnFsbjJlMHRnbmFxIn0.9jRwEDR0xab-xftbdq7t7Q';
+  const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11', //  map style
+      //center: [-82.25306449222539, 9.40445610762273], // center
+      zoom: 11 // set zoom level
+  });
+
+  document.querySelectorAll('.special').forEach(function (special) {
+      const latitude = parseFloat(special.dataset.coordinates.split(',')[0]);
+      const longitude = parseFloat(special.dataset.coordinates.split(',')[1]);
+
+      // Create a marker
+      const marker = new mapboxgl.Marker()
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+
+      // Create a popup
+      const popup = new mapboxgl.Popup({ offset: 25 })
+          .setText(special.querySelector('h3').textContent) // Set the popup text to the h3 content
+          .setMaxWidth('300px'); // Optional: Set max width of the popup
+
+      // Add the popup to the marker
+      marker.setPopup(popup);
+
+      // Optional: Add event listener for showing the popup on marker click
+      marker.getElement().addEventListener('click', function () {
+          marker.togglePopup();
+      });
+  });
+}
