4d7aee34786fd3ab651e7893460c30fa19b60f2f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-09 13:50:38 +0000

    Add swapping_img js for event clikc on smaller img

---
+++ assets/js/swaping_img.js
@@ -0,0 +1,21 @@
+
+  document.addEventListener('DOMContentLoaded', function () {
+   
+    const photoSection = document.querySelector('.photos');
+
+    /*Add a click event listener to the section */
+    photoSection.addEventListener('click', function (event) {
+        /*clicked element is the smaller image */
+        if (event.target.matches('.smaller')) {
+            // Replace the source of the bigger image with the smaller image
+            const biggerImg = document.getElementById('mainImg');
+            const smallerImg = document.getElementById('notMainImg');
+            const tempSrc = biggerImg.src;
+            const tempAlt = biggerImg.alt;
+            biggerImg.src = smallerImg.src;
+            biggerImg.alt = smallerImg.alt;
+            smallerImg.src = tempSrc;
+            smallerImg.alt = tempAlt;
+        }
+    });
+});
