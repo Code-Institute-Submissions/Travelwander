ccd9470628d7e7a7781dbe740ff7c1d172a837e6
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-12 18:48:11 +0000

    Add mapview update function

--- assets/js/mapview.js
+++ assets/js/mapview.js
@@ -1,4 +1,4 @@
- const mainContent = document.getElementById('mainContent');
+/*  const mainContent = document.getElementById('mainContent');
 const mapContainer = document.getElementById('map');
 const mapView = document.getElementById('mapview');
 const windowWidth = window.innerWidth;
@@ -37,3 +37,33 @@ if (windowWidth < 900) {
     }
     mapView.addEventListener('click', toggleView);
 
+ */
+    const mainContent = document.getElementById('mainContent');
+    const mapContainer = document.getElementById('map');
+    const mapView = document.getElementById('mapview');
+    const windowWidth = window.innerWidth;
+    
+    // Initial state based on window width
+    if (windowWidth < 900) {
+      mapContainer.style.display = 'none';
+    } else {
+      mapContainer.style.display = 'block';
+    }
+    
+    // Get the main content, map view
+    function toggleView() {
+      // Toggle visibility
+      const specialElements = document.querySelectorAll('.special-container');
+      specialElements.forEach(element => {
+        element.style.display = element.style.display === 'none' ? 'flex' : 'none';
+      });
+    
+      // Show/hide the map
+      mapContainer.style.display = mapContainer.style.display === 'none' ? 'block' : 'none';
+    
+      // Map view text depends on mapContainer display
+      mapView.innerHTML = mapContainer.style.display === 'none' ? 'Map View <br> <i class="fa-solid fa-map-location-dot">' : 'List View <br> <i class="fa-solid fa-list"></i>';
+    }
+    
+    mapView.addEventListener('click', toggleView);
+    
