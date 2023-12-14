f97887d66247b199baacd787789940d59c48f6dc
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-14 00:10:33 +0000

    Add arrays of courntries for show flags imgs

---
+++ assets/js/flags.js
@@ -0,0 +1,3 @@
+const asia = ['Japan', 'Thailand', 'South Korea', 'Indonesia'];
+const europe = ['France', 'Austria', 'Greece', 'Spain'];
+const North_america = ['USA', 'Jamaica', 'Panama', 'Mexico'];
