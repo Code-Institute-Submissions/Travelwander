e6e64e92a483569a9d54ff5ebd9503347536d887
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-15 15:20:50 +0000

    Update modal and add form to user enter details

--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,33 +1,41 @@
- 
- document.addEventListener('DOMContentLoaded', function () {
 
- 
- /* reserve button */
- const reserveBtn = document.getElementById('reserveBtn');
- reserveBtn.innerText = 'Reserve Now';
+document.addEventListener('DOMContentLoaded', function () {
+  const reserveBtn = document.getElementById('reserveBtn');
+  const reservationModal = document.getElementById('reservationModal');
+  const reservationForm = document.getElementById('reservationForm');
+  const confirmationIcon = document.getElementById('confirmationIcon');
+  const confirmationMessage = document.getElementById('confirmationMessage');
+  const reservationNumberSpan = document.getElementById('res_number');
 
- reserveBtn.addEventListener('click', () => {
-   // Add logic for handling reservation, if needed
+  reserveBtn.addEventListener('click', () => {
+    reservationModal.style.display = 'block';
+  });
 
-   // Display confirmation message in the modal
-   const confirmationIcon =document.getElementById('confirmationIcon');
-   const confirmationModal = document.getElementById('confirmationModal');
-   const confirmationMessage = document.getElementById('confirmationMessage');
-   const reservationNumberSpan = document.getElementById('res_number');
+  reservationForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-   /* create random number */
-   const randomReservationNumber = Math.floor(Math.random() * (4055555 - 250000 + 1)) + 250000;
+    console.log('Form submitted'); // Check if the form submission event is triggered
 
-   confirmationIcon.innerHTML =`<i class="fa-solid fa-circle-check"></i>`;
-   confirmationMessage.innerText = `Reservation confirmed!`;
-   reservationNumberSpan.innerText = randomReservationNumber;
-   confirmationModal.style.display = 'block';
+    // Validate and process the form data
+    // For simplicity, let's generate a random reservation number
+    const randomReservationNumber = Math.floor(Math.random() * (4055555 - 250000 + 1)) + 250000;
 
-   // Close the modal if the close button is clicked
-   const closeButton = document.querySelector('.close');
-   closeButton.addEventListener('click', () => {
-     confirmationModal.style.display = 'none';
-   });
- });
+    confirmationIcon.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+    confirmationMessage.innerText = `Reservation confirmed! and one of our expert will call you !`;
+    reservationNumberSpan.innerText = randomReservationNumber;
 
+    // Optionally, you can send the form data to a server for further processing
+
+    // Close the modal after submission
+    reservationModal.style.display = 'block'; // Change 'none' to 'block'
+    reservationForm.style.display = 'none';
+    // Optionally, reset the form
+    reservationForm.reset();
+  });
+
+  const closeButton = document.querySelector('.close');
+  closeButton.addEventListener('click', () => {
+    reservationModal.style.display = 'none';
+  });
 });
+
