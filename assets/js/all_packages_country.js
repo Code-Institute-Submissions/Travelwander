4dd17095dff9f2c15a549a3068491a75e1a705aa
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-15 21:13:36 +0000

    Update api tocken

--- assets/js/all_packages_country.js
+++ assets/js/all_packages_country.js
@@ -94,7 +94,7 @@ function displayHotels(hotels) {
 
 /* Map initialization function */
 function initializeMap(centerCoordinates) {
-  mapboxgl.accessToken = 'pk.eyJ1IjoibWFpc2FtMjAwNCIsImEiOiJjbHBsZTJrYTcwM3lmMnFsbjJlMHRnbmFxIn0.9jRwEDR0xab-xftbdq7t7Q';
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWFpc2FtMjAwNCIsImEiOiJjbHBsZTJrYTcwM3lmMnFsbjJlMHRnbmFxIn0.9jRwEDR0xab-xftbdq7t';
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', //  map style
