f111c09272f3b890be06ad86cd3a6f5a7e7c589a
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-20 13:28:36 +0000

    Add script homelink to logo to act as link

---
+++ assets/js/homelink.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const logoImage = document.getElementById('logoImage');
+    const homeLink = document.getElementById('homeLink');
+
+    logoImage.addEventListener('click', function () {
+      // Trigger a click on the home link
+      homeLink.click();
+    });
+  });
