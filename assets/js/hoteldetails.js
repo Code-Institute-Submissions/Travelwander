7237323b87c92395ae254a7922b451010513f547
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-20 15:03:17 +0000

    Update page script to hoteldetails.js

---
+++ assets/js/hoteldetails.js
@@ -0,0 +1,93 @@
+
+    const all_icons = [
+      "fas fa-hotel",
+      "fas fa-bed",
+      "fas fa-suitcase",
+      "fas fa-briefcase",
+      "fas fa-swimmer",
+      "fas fa-sun",
+      "fas fa-beach-ball",
+      "fas fa-cocktail",
+      "fas fa-palm-tree",
+      "fas fa-umbrella-beach",
+      "fas fa-ship",
+      "fas fa-plane",
+      "fas fa-train",
+      "fas fa-car",
+      "fas fa-bicycle",
+      "fas fa-hiking",
+      "fas fa-camera",
+      "fas fa-utensils",
+      "fas fa-coffee",
+      "fas fa-ice-cream",
+      "fa-solid fa-sleigh",
+      "fa-solid fa-champagne-glasses",
+      "fa-solid fa-plane-arrival",
+      "fa-solid fa-suitcase-rolling",
+      "fa-solid fa-table-tennis-paddle-ball",
+      "fa-solid fa-place-of-worship"
+
+    ]
+
+    const selectedHotelIndex = sessionStorage.getItem('selectedHotelIndex'); /* Retrieve index from session storage */
+    let first_coord =[]
+    if (selectedHotelIndex !== null) {
+
+      fetch('hotels_data.json') /* Fetch and parse the JSON data */
+        .then(response => response.json())
+        .then(data => {
+          /*  selected hotel data */
+          const selectedHotel = data[selectedHotelIndex];
+          //document.getElementsByClassName('big_img').innerHTML=`<img src="${selectedHotel.imageSrc[0]}"`
+          document.getElementById('big_img').innerHTML = `<img alt="${selectedHotel.imageSrc[0]}" class="bigger" id="mainImg" src="${selectedHotel.imageSrc[0]}">`;
+          document.getElementById('small_img').innerHTML = `<img alt="${selectedHotel.imageSrc[1]}" class="smaller"  id="notMainImg" src="${selectedHotel.imageSrc[1]}"><p class="price_tag">${selectedHotel.price}<span>per person</span></p>`;
+
+
+          document.getElementById('hotelName').innerText = selectedHotel.name;/*Update HTML elements */
+
+          document.getElementById('hotelRating').innerText = `Rating: ${selectedHotel.rating}`;
+
+          document.getElementById('hotelLocation').innerText = `Location: ${selectedHotel.location}, ${selectedHotel.country}`;
+          
+          document.getElementById('hotelDescription').innerText = selectedHotel.description;
+          //document.getElementById('hotelDetails').innerText = `${selectedHotel.details.join('')}`;
+
+          document.getElementById('hotelDetails').innerHTML = `${selectedHotel.details.join('<br>')}`;
+          /* but for icons we use try to get hotel Icons id */
+          const iconsContainer = document.getElementById('hotelIcons');
+          selectedHotel.details.forEach(detail => {
+            const icon = document.createElement('span');
+            const randomIcon = all_icons[Math.floor(Math.random() * all_icons.length)];
+            icon.className = randomIcon;
+            iconsContainer.appendChild(icon);
+          });
+          /* price */
+          document.getElementById('hotelPrice').innerText = `Price: ${selectedHotel.price}`;
+          //document.getElementById('hotelPrice2').innerText = `Price: ${selectedHotel.price}
+          /* reserve button */
+          first_coord=selectedHotel.coordinates.split(',')
+          const [longitude, latitude] = first_coord;
+          initializeMap([longitude, latitude]);
+
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    } else {
+      console.error('No hotel selected.');
+    }
+/* */
+function initializeMap(coordinates) {
+    mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'; // Replace with your Mapbox access token
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: coordinates,
+      zoom: 15,
+    });
+
+    // Add a marker
+    new mapboxgl.Marker()
+      .setLngLat(coordinates)
+      .addTo(map);
+  }
+   
