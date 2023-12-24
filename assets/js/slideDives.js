811d90e55831731c29ed04da39e61738e1eb2d1d
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-24 15:14:25 +0000

    delet dive js contents

--- assets/js/slideDives.js
+++ assets/js/slideDives.js
@@ -1,51 +1,6 @@
-/* 
+
 //mini slide at index landing part
-document.addEventListener('DOMContentLoaded', function () {
-    const usSlides = document.querySelectorAll('.namerica');
-    const euSlides = document.querySelectorAll('.europe');
-    const asiaSlides = document.querySelectorAll('.asia');
 
-    function createSlideshow(slide, images) {
-      let currentIndex = 0;
-
-      function showImage() {
-        slide.style.backgroundImage = `url(${images[currentIndex]})`;
-        currentIndex = (currentIndex + 1) % images.length;
-      }
-
-      const slideshowInterval = setInterval(showImage, 5000); 
-
-      // Pause 
-      slide.addEventListener('mouseover', () => clearInterval(slideshowInterval));
-
-      // Resume 
-      slide.addEventListener('mouseout', () => {
-        clearInterval(slideshowInterval);
-        slideshowInterval = setInterval(showImage, 5000);
-      });
-    }
-
-    usSlides.forEach(slide => {
-      const images = [
-        'assets/images/miami1.jpg', 'assets/images/miami2.jpg', 'assets/images/miami3.jpg'
-      ];
-      createSlideshow(slide, images);
-    });
-
-    euSlides.forEach(slide => {
-      const images = [
-        'assets/images/eu1.jpg', 'assets/images/eu2.jpg', 'assets/images/eu3.jpg'
-      ];
-      createSlideshow(slide, images);
-    });
-
-    asiaSlides.forEach(slide => {
-      const images = [
-        'assets/images/asia1.jpg', 'assets/images/asia2.jpg', 'assets/images/asia3.jpg'
-      ];
-      createSlideshow(slide, images);
-    });
-  }); */
 
   document.addEventListener('DOMContentLoaded', function () {
     const usSlides = document.querySelectorAll('.namerica');
