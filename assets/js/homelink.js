8352c9d094cc1ae8d5eb757433fe8a11f5dde477
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-12-24 13:16:35 +0000

    Update homelink js for github logo link

--- assets/js/homelink.js
+++ assets/js/homelink.js
@@ -1,10 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
+/* document.addEventListener('DOMContentLoaded', function () {
     const logoImage = document.getElementById('logoImage');
     const homeLink = document.getElementById('homeLink');
 
     logoImage.addEventListener('click', function () {
       // Trigger a click on the home link
-      homeLink = 'Travelwander/'+homeLink;
       homeLink.click();
     });
-  });
+  }); */
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const logoImage = document.getElementById('logoImage');
+    const homeLink = document.getElementById('homeLink');
+
+    // Prepend "Travelwander/" to the existing href
+    homeLink.href = "Travelwander/" + homeLink.href;
+
+    // Add a click event listener to the logo image
+    logoImage.addEventListener('click', function () {
+        // Trigger a click on the modified home link
+        homeLink.click();
+    });
+});
